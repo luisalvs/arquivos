f4b9f8029305acd8491310c47fa4c86a67c14031
luisalvs <luisalvst@gmail.com>
2025-08-28 16:19:20 -0300

    Create numero_favorito.py

---
+++ numero_favorito.py
@@ -0,0 +1,25 @@
+import json
+
+# username = input("What is your name? ")
+filename = 'username.json'
+
+# with open(filename, 'w') as f_obj:
+#     json.dump(username, f_obj)
+# print("We'll remember you when you come back, " + username + "!") 
+
+# with open(filename) as f_obj:
+#     username = json.load(f_obj)
+# print("Welcome back, " + username + "!") 
+
+try:
+    with open(filename) as f_obj:
+        username = json.load(f_obj)
+
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+        print("We'll remember you when you come back, " + username + "!")
+
+else:
+    print("Welcome back, " + username + "!")
